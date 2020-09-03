03f63d83c72c53449ad00c90aba77b0ef3b8f8a6
Ha Minh Tung <hmtung@tma.com.vn>
2020-09-03 18:36:28 +0700

    at least we tried. Need to work more on moving camera

--- GoDodge/Assets/Scripts/Camera/CameraScript.cs
+++ GoDodge/Assets/Scripts/Camera/CameraScript.cs
@@ -1,34 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
 	public Transform player;
-	public Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		var screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
 
-    // Update is called once per frame
-    void Update()
-    {
+		//Debug.Log($"Camera position: {transform.position.x}");
 		var xVector = player.position.x;
 		var yVector = player.position.y;
 
-		if(xVector < -1 || xVector > 1)
-		{
-			xVector = transform.position.x;
-		}
+		//Debug.Log($"Player position: {xVector}");
+		//Vector3 screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+		//Debug.Log($"screenPos position X: {screenBounds.x}");
+		//if (xVector < -1 || xVector > 1) // << This muse be calculate to fit all screen
+		//{
+		//	xVector = transform.position.x;
+		//}
 
-		if(yVector < 0 || yVector > 11)
+		if (yVector < 0 || yVector > 11)
 		{
 			yVector = transform.position.y;
 		}
 
-		transform.position = new Vector3(xVector, yVector, transform.position.z); 
+		transform.position = new Vector3(xVector, yVector, transform.position.z);
 	}
 }
